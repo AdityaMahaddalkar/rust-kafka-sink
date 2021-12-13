66345a59b01bd8a97e9261477b4767fbc5a38ae0
Aditya Mahaddalkar <adityam1311@gmail.com>
2021-12-13 19:05:23 +0530

    Added  rdkafka crate and created a simple consumer

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rust-kafka-sink
+
 PoC application for consuming from Kafka and storing in Postgres DB using Rust
+
+## Quick Start
+
+- Spin up kafka using `docker-compose up -d -f kafka-docker-compose.yaml`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use log::{info, warn};
+use rdkafka::{consumer::{BaseConsumer, Consumer}, ClientConfig, Message};
+
 fn main() {
-    println!("Hello, world!");
+    consume_and_print("localhost:29092", "rust-app", &vec!["rust-kafka"]);
+}
+
+fn consume_and_print(brokers: &str, group_id: &str, topics: &Vec<&str>) {
+
+    let consumer = ClientConfig::new()
+        .set("group.id", group_id)
+        .set("bootstrap.servers", brokers)
+        .set("enable.auto.commit", "true")
+        .expect("Consiumer creation failed");
+
+    consumer.subscribe(topics)
+        .expect("Can't subscribe to specified topics");
+
+    loop {
+        match consumer.recv().await {
+            Err(e) => warn!("Kafka error: {}". e),
+            Ok(m) => {
+                let payload = match m.payload_view::<str>() {
+                    None => "",
+                    Some(Ok(s)) => s,
+                    Some(Err(e)) => {
+                        warn!("Error while deserializing message payload {:?}", e);
+                        ""
+                    }
+                };
+                info!("payload: {}");
+                consumer.commit_message(&m, CommitMode::Async).unwrap();
+            }
+        }
+    }
 }
